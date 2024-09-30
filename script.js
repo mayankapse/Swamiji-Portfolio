98a217c9f4dea6dc0def4d9721b2d4f043ddb7c6
mayankapse <mayankapse1304@gmail.com>
2024-09-30 23:43:50 +0530

    Changed Files

--- script.js
+++ script.js
@@ -43,73 +43,22 @@ function highlightNavLink() {
 window.addEventListener("scroll", highlightNavLink);
 window.addEventListener("load", highlightNavLink);
 
-// Automatically slide images every 3 seconds
-function moveSlide(step) {
-  const slides = document.querySelector(".slider");
+// courses section
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
-  if (slides) {
-    const totalSlides = slides.querySelectorAll(".slide").length;
-    currentIndex += step;
-
-    if (currentIndex < 0) {
-      currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-      currentIndex = 0;
-    }
-
-    const offset = -currentIndex * 100;
-    slides.style.transform = `translateX(${offset}%)`;
-  }
-}
-
-function highlightNavLink() {
-  const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll("nav a");
-
-  let currentSectionId = "";
-
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
-      currentSectionId = section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href").substring(1) === currentSectionId) {
-      link.classList.add("active");
-    }
-  });
-}
-
-// Add event listener for scroll to highlight the current section
-window.addEventListener("scroll", highlightNavLink);
-
-// Call the function on page load to set the correct active link
-window.addEventListener("load", highlightNavLink);
-
-// Automatically slide images every 3 seconds
-const slideInterval = 5000; // Time interval in milliseconds
-setInterval(() => {
-  moveSlide(1);
-}, slideInterval);
-
-document.querySelector("#contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = "";
-  e.target.elements.email.value = "";
-  e.target.elements.message.value = "";
+next.addEventListener('click', function() {
+  let items = document.querySelectorAll('.item');
+  document.querySelector('.slide').appendChild(items[0]);
 });
 
-// knowldge session 
-function showMoreText(event1) {
-  event.preventDefault();
-  window.open("stotra.html", "_self");
-}
+prev.addEventListener('click', function() {
+  let items = document.querySelectorAll('.item');
+  document.querySelector('.slide').prepend(items[items.length - 1]);
+});
 
-function showMoreText(event2) {
+// knowledge session
+function showMoreText(event, page) {
   event.preventDefault();
-  window.open("pitra-dosha.html", "_self");
+  window.open(page, "_self");
 }
